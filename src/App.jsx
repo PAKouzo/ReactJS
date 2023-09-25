0584e6c5a793bcfc145c6dcd342dbf2cc6c62d13
PAKouzo <dohieuxk14@gmail.com>
2023-09-25 14:51:44 +0700

    HTTP

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 const App = () => {
   const [count, setCount] = useState(1)
   const handleClick = () =>{
     setCount(count+1)
   }
+  const handleFetchPokemon = async () =>{
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+    console.log(response)
+  }
   return (
     <div>
       {count}
       <br />
       <button onClick={handleClick}>Click</button>
+      <button onClick={handleFetchPokemon}>Click</button>
     </div>
   )
 }
---
+++ src/components/Phukien.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios'
+import ReactLoading from 'react-loading'
+const Phukien = () => {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Phukien
